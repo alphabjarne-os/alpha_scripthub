c2cbce562ec7e0fe904d30c11d0bb9ba711127bf
alphabjarne-os <bjarne.fokuhl@gmail.com>
2026-06-23 07:26:31 +0200

    Add Anti-AFK toggle to the top of Main tab using Player.Idled and VirtualUser

--- games/107646426076756.lua
+++ games/107646426076756.lua
@@ -2,6 +2,16 @@ local Window = _G.AlphaWindow
 local currentExecId = _G.AlphaScriptExecutionId
 local player = game.Players.LocalPlayer
 local myPlot = nil
+local AntiAFKEnabled = false
+
+local virtualUser = game:GetService("VirtualUser")
+player.Idled:Connect(function()
+    if AntiAFKEnabled then
+        virtualUser:Button2Down(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
+        task.wait(1)
+        virtualUser:Button2Up(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
+    end
+end)
 
 local function findMyPlot()
     local map = workspace:FindFirstChild("Map")
@@ -74,6 +84,15 @@ end
 
 local MainTab = Window:CreateTab("Main", 4483362458)
 MainTab:CreateSection("Automation")
+
+MainTab:CreateToggle({
+    Name = "Anti-AFK",
+    CurrentValue = false,
+    Flag = "AlphaMainAntiAFKToggle",
+    Callback = function(Value)
+        AntiAFKEnabled = Value
+    end,
+})
 
 local AutoSellEnabled = false
 MainTab:CreateToggle({
